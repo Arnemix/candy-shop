b0c613e5fcce0f84e9bb7a70a4fe8a6da87eb6ae
Arnemix <contact.arnemix@gmail.com>
2024-02-16 15:16:39 +0100

    update default user name + add sign out button

--- src/layouts/navbar/NavBar.jsx
+++ src/layouts/navbar/NavBar.jsx
@@ -7,7 +7,7 @@ import SignInModal from "../../components/SignIn/SIgnIn";
 import "./NavBar.scss";
 
 function NavBar(props) {
-    const [user, setUser] = useState("Guest");
+    const [user, setUser] = useState();
     const [showSignInModal, setShowSignInModal] = useState(false);
 
     const fctSignIn = () => {
@@ -48,13 +48,15 @@ function NavBar(props) {
             </div>
             <div className="buttonGroup">
                 <div id="userMarge">
-                    <p>User : {user ? user.firstname : ""}</p>
+                    <p>User : {user ? user.firstname : "Guest"}</p>
                 </div>
-                {!user && (
+                {!user ? (
                     <>
                         <button onClick={fctSignIn}>Sign in</button>
                         {showSignInModal && <SignInModal onClose={() => setShowSignInModal(false)} onSignIn={setUser} />}
                     </>
+                ) : (
+                    <button onClick={() => setUser(null)}>Sign out</button>
                 )}
             </div>
         </div>
